6461422f3bdde09182418a1457a01a010f5ebf77
hsty01009-create <hsty01009@gmail.com>
2026-06-19 15:36:18 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
+BOT_TOKEN = "توکن_ربات"
+CREATOR = "🇮🇷امیر علی فروزان اصل🇮🇷"
