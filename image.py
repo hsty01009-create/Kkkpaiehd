1f9bb86fe4856f9525cce0c41c4ecd62a982f0fa
hsty01009-create <hsty01009@gmail.com>
2026-06-19 19:16:54 +0330

    Update image.py

--- image.py
+++ image.py
@@ -1,27 +1,17 @@
-from PIL import Image, ImageDraw
-
-CREATOR = "امیر علی فروزان اصل"
+from PIL import Image, ImageEnhance
 
 def edit_image(path):
     img = Image.open(path)
-    d = ImageDraw.Draw(img)
-
-    d.rectangle([(0,0),(800,100)], fill="black")
-    d.text((20,30), "EDITED ✨", fill="white")
-
-    d.rectangle([(0,450),(800,500)], fill="black")
-    d.text((20,460), CREATOR, fill="white")
-
+    img = ImageEnhance.Brightness(img).enhance(1.3)
+    img = ImageEnhance.Contrast(img).enhance(1.4)
     out = "edit.jpg"
     img.save(out)
     return out
 
 
 def make_sticker(path):
-    img = Image.open(path)
-    img = img.convert("RGBA")
-    img = img.resize((512,512))
-
+    img = Image.open(path).convert("RGBA")
+    img.thumbnail((512,512))
     out = "sticker.webp"
     img.save(out, "WEBP")
     return out
