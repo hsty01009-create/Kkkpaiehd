b5bb36f09dc41f1a9b49efc0d04d3b446d64c2e6
hsty01009-create <hsty01009@gmail.com>
2026-06-19 22:51:59 +0330

    Update texts.py

--- texts.py
+++ texts.py
@@ -1,16 +1,14 @@
 LANGS = {
     "fa": "فارسی",
     "en": "English",
-    "ar": "Arabic",
+    "ar": "العربية",
+    "ru": "Русский",
     "tr": "Türkçe",
     "de": "Deutsch",
-    "ru": "Русский",
     "fr": "Français"
 }
 
 def welcome(lang, coins):
     if lang == "fa":
-        return f"سلام 👋\n💰 سکه: {coins}"
-    if lang == "en":
-        return f"Hello 👋\n💰 Coins: {coins}"
-    return f"Welcome 👋\nCoins: {coins}"
+        return f"👋 خوش آمدی!\n💰 سکه: {coins}"
+    return f"Welcome!\nCoins: {coins}"
