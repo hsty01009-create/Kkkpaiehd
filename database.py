a1ea19d018ed7b1ea571002a0d917e62eed28fe3
hsty01009-create <hsty01009@gmail.com>
2026-06-19 23:19:22 +0330

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,8 @@ CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY,
     coins INTEGER DEFAULT 0,
     lang TEXT DEFAULT 'fa',
-    invited_by INTEGER
+    invited_by INTEGER,
+    last_daily INTEGER DEFAULT 0
 )
 """)
 
@@ -18,14 +19,11 @@ conn.commit()
 def add_user(user_id, invited_by=None):
     cur.execute("SELECT id FROM users WHERE id=?", (user_id,))
     if not cur.fetchone():
-        cur.execute("INSERT INTO users (id, coins, invited_by) VALUES (?, ?, ?)",
-                    (user_id, 0, invited_by))
+        cur.execute("""
+        INSERT INTO users (id, coins, invited_by)
+        VALUES (?, ?, ?)
+        """, (user_id, 0, invited_by))
         conn.commit()
-
-
-def get_user(user_id):
-    cur.execute("SELECT * FROM users WHERE id=?", (user_id,))
-    return cur.fetchone()
 
 
 def add_coins(user_id, amount):
@@ -33,12 +31,11 @@ def add_coins(user_id, amount):
     conn.commit()
 
 
+def get_user(user_id):
+    cur.execute("SELECT * FROM users WHERE id=?", (user_id,))
+    return cur.fetchone()
+
+
 def set_lang(user_id, lang):
     cur.execute("UPDATE users SET lang=? WHERE id=?", (lang, user_id))
     conn.commit()
-
-
-def get_lang(user_id):
-    cur.execute("SELECT lang FROM users WHERE id=?", (user_id,))
-    r = cur.fetchone()
-    return r[0] if r else "fa"
