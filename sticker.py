42c6e3a94c9f3dbdc9e97e7b10ce411eafcc9ace
hsty01009-create <hsty01009@gmail.com>
2026-06-19 22:31:42 +0330

    Update sticker.py

--- sticker.py
+++ sticker.py
@@ -1,8 +1,10 @@
 from PIL import Image
 
 def create_sticker(path):
-    img = Image.open(path).convert("RGBA")
-    img = img.resize((512, 512))
+    img = Image.open(path)
+    img = img.convert("RGBA")
+    img.thumbnail((512, 512))
+
     out = "sticker.webp"
     img.save(out, "WEBP")
     return out
