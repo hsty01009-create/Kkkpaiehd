26d0b3fa346ea69774041718532316367263f4d3
hsty01009-create <hsty01009@gmail.com>
2026-06-19 17:50:26 +0330

    Update image.py

--- image.py
+++ image.py
@@ -2,28 +2,26 @@ from PIL import Image, ImageDraw
 
 CREATOR = "امیر علی فروزان اصل"
 
-def make_image(text):
-    img = Image.new("RGB", (800, 500), (30, 30, 30))
-    draw = ImageDraw.Draw(img)
-
-    draw.text((100, 200), text, fill="white")
-    draw.text((200, 450), CREATOR, fill="gray")
-
-    out = "img.jpg"
-    img.save(out)
-    return out
-
-
 def edit_image(path):
     img = Image.open(path)
     draw = ImageDraw.Draw(img)
 
-    draw.rectangle([(0,0),(800,80)], fill=(0,0,0))
-    draw.text((20,20), "EDITED ✨", fill="white")
+    draw.rectangle([(0,0),(800,100)], fill="black")
+    draw.text((20,30), "EDITED ✨", fill="white")
 
-    draw.rectangle([(0,450),(800,500)], fill=(0,0,0))
+    draw.rectangle([(0,450),(800,500)], fill="black")
     draw.text((20,460), CREATOR, fill="white")
 
     out = "edit.jpg"
     img.save(out)
+    return out
+
+
+def make_sticker(path):
+    img = Image.open(path)
+    img = img.convert("RGBA")
+    img = img.resize((512,512))
+
+    out = "sticker.webp"
+    img.save(out, "WEBP")
     return out
