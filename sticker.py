2a7fbda76468c9b7e8968ec841e458319f8004bb
hsty01009-create <hsty01009@gmail.com>
2026-06-19 21:20:53 +0330

    Update sticker.py

--- sticker.py
+++ sticker.py
@@ -1,8 +1,34 @@
 from PIL import Image
+import os
 
-def make_sticker(path):
-    img = Image.open(path).convert("RGBA")
-    img.thumbnail((512, 512))
-    out = path.replace(".jpg", ".webp")
-    img.save(out, "WEBP")
-    return out
+
+def create_sticker(input_file, output_file="sticker.webp"):
+    """
+    تبدیل عکس به استیکر تلگرام
+    """
+
+    img = Image.open(input_file).convert("RGBA")
+
+    width, height = img.size
+
+    max_size = 512
+
+    if width > height:
+        new_width = max_size
+        new_height = int(height * max_size / width)
+    else:
+        new_height = max_size
+        new_width = int(width * max_size / height)
+
+    img = img.resize((new_width, new_height))
+
+    background = Image.new("RGBA", (512, 512), (0, 0, 0, 0))
+
+    x = (512 - new_width) // 2
+    y = (512 - new_height) // 2
+
+    background.paste(img, (x, y), img)
+
+    background.save(output_file, "WEBP")
+
+    return output_file
