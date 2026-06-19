aaa769488f419e33adc637b2b11691cea8d62ff7
hsty01009-create <hsty01009@gmail.com>
2026-06-19 16:47:28 +0330

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,41 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+def menu():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="🖼 ساخت عکس", callback_data="img")],
+        [InlineKeyboardButton(text="✏️ ادیت عکس", callback_data="edit")],
+        [InlineKeyboardButton(text="🧊 استیکر", callback_data="sticker")],
+        [InlineKeyboardButton(text="💰 سکه", callback_data="coins")],
+        [InlineKeyboardButton(text="🎁 روزانه", callback_data="daily")],
+        [InlineKeyboardButton(text="👥 دعوت", callback_data="invite")]
+    ])
+
+
+def rules():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="✅ قبول", callback_data="accept"),
+            InlineKeyboardButton(text="❌ رد", callback_data="reject")
+        ]
+    ])
+
+
+# 🌍 7 زبان
+def lang():
+    return InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="🇮🇷 فارسی", callback_data="fa"),
+            InlineKeyboardButton(text="🇺🇸 English", callback_data="en")
+        ],
+        [
+            InlineKeyboardButton(text="🇷🇺 Русский", callback_data="ru"),
+            InlineKeyboardButton(text="🇪🇸 Español", callback_data="es")
+        ],
+        [
+            InlineKeyboardButton(text="🇮🇳 हिन्दी", callback_data="hi"),
+            InlineKeyboardButton(text="🇹🇷 Türkçe", callback_data="tr")
+        ],
+        [
+            InlineKeyboardButton(text="🇫🇷 Français", callback_data="fr")
+        ]
+    ])
