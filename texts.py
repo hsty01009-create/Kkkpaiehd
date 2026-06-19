f135304e61f1dc8642a6079b5756f6fb0bb1ab97
hsty01009-create <hsty01009@gmail.com>
2026-06-19 20:02:13 +0330

    Create texts.py

---
+++ texts.py
@@ -0,0 +1,20 @@
+LANGS = {
+    "fa": "فارسی",
+    "en": "English",
+    "fr": "Français",
+    "es": "Español",
+    "ar": "العربية",
+    "tr": "Türkçe",
+    "ru": "Русский"
+}
+
+def welcome(lang, coins):
+    return f"""
+✨ خوش آمدی
+
+🌍 زبان: {lang}
+💰 سکه: {coins}
+
+━━━━━━━━━━━━
+🤖 Pro Bot
+"""
