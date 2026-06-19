1c0fead6f4a0263c7a6b53957fae8c259381b319
hsty01009-create <hsty01009@gmail.com>
2026-06-19 04:12:18 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel("gemini-3.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 CREATOR = "Amir Ali Forouzan"
 
