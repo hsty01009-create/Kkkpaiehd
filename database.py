5f328c5e0f1d8742af782f5e3639fda19ebe5976
hsty01009-create <hsty01009@gmail.com>
2026-06-19 23:10:44 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-
 import sqlite3
 
 conn = sqlite3.connect("bot.db", check_same_thread=False)
@@ -6,35 +5,40 @@ cur = conn.cursor()
 
 cur.execute("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
-    coins INTEGER DEFAULT 100,
+    id INTEGER PRIMARY KEY,
+    coins INTEGER DEFAULT 0,
     lang TEXT DEFAULT 'fa',
     invited_by INTEGER
 )
 """)
+
 conn.commit()
 
+
 def add_user(user_id, invited_by=None):
-    cur.execute("SELECT user_id FROM users WHERE user_id=?", (user_id,))
+    cur.execute("SELECT id FROM users WHERE id=?", (user_id,))
     if not cur.fetchone():
-        cur.execute(
-            "INSERT INTO users (user_id, invited_by) VALUES (?,?)",
-            (user_id, invited_by)
-        )
+        cur.execute("INSERT INTO users (id, coins, invited_by) VALUES (?, ?, ?)",
+                    (user_id, 0, invited_by))
         conn.commit()
 
-def get_coins(user_id):
-    cur.execute("SELECT coins FROM users WHERE user_id=?", (user_id,))
-    return cur.fetchone()[0]
+
+def get_user(user_id):
+    cur.execute("SELECT * FROM users WHERE id=?", (user_id,))
+    return cur.fetchone()
+
 
 def add_coins(user_id, amount):
-    cur.execute("UPDATE users SET coins = coins + ? WHERE user_id=?", (amount, user_id))
+    cur.execute("UPDATE users SET coins = coins + ? WHERE id=?", (amount, user_id))
     conn.commit()
+
 
 def set_lang(user_id, lang):
-    cur.execute("UPDATE users SET lang=? WHERE user_id=?", (lang, user_id))
+    cur.execute("UPDATE users SET lang=? WHERE id=?", (lang, user_id))
     conn.commit()
 
+
 def get_lang(user_id):
-    cur.execute("SELECT lang FROM users WHERE user_id=?", (user_id,))
-    return cur.fetchone()[0]
+    cur.execute("SELECT lang FROM users WHERE id=?", (user_id,))
+    r = cur.fetchone()
+    return r[0] if r else "fa"
